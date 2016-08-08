45f6da61a388e85464abe177fa2be8768bbef9d3
Azeezah <azeezahm14@gmail.com>
2016-08-08 16:45:21 -0400

    Remove outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 
-def outlierCleaner(predictions, ages, net_worths):
+def outlierCleaner(predictions, ages, net_worths, percent_outliers=10):
     """
         Clean away the 10% of points that have the largest
         residual errors (difference between the prediction
@@ -14,7 +14,12 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    errors = [abs(n-p)**2 for n, p in zip(net_worths, predictions)]
+    num_outliers = round(len(ages)*percent_outliers/100)
 
+    cleaned_data = zip(ages, net_worths, errors)
+    cleaned_data = sorted(cleaned_data, key=lambda x:x[2])
+    cleaned_data = cleaned_data[:-num_outliers:]
     
     return cleaned_data
 
